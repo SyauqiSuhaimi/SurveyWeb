f916b1743db129f2d1257c2ed086d12c6adadc48
SyauqiSuhaimi <muhammad.syauqi@aoikumo.com>
2025-05-15 18:19:37 +0800

    Update Program.cs

--- SurveyWeb/Program.cs
+++ SurveyWeb/Program.cs
@@ -3,16 +3,30 @@ using SurveyWeb.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ISurveyServices, SurveyServices>();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowExpoApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:8081") 
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials(); 
+    });
+});
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
+
+app.UseCors("AllowExpoApp");
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
